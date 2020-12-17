b4cd851e05cc72e56847f45043fbc23506ffe109
lincey-cherian-au7 <linceycherian@gmail.com>
2020-12-17 18:29:39 +0530

    Added a reusable content

--- src/components/TrelloList/TrelloList.js
+++ src/components/TrelloList/TrelloList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './TrelloList.css';
 import TrelloCard from '../TrelloCard/TrelloCard'
-
+import TrelloActionButton from '../TrelloActionButton/TrelloActionButton'
 const TrelloList = ({title,cards})=>{
     return(
        <div className="list_container">
            <h3>{title}</h3>
            {cards.map(card=>
-           <TrelloCard text={card.text}/>)}
+           <TrelloCard key={card.id} text={card.text}/>)}
+           <TrelloActionButton/>
        </div>
     )
 }
